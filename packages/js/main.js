f8f46a5bace3d13e1bddc3db21bd0645978f2f4c
arsh459 <arshgoyal459@gmail.com>
2023-05-18 12:38:56 +0530

    update ids

--- packages/js/main.js
+++ packages/js/main.js
@@ -158,23 +158,21 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 const sections = document.querySelectorAll("section[id]")
 
 function scrollActive() {
-  const scrollY = window.pageYOffset
-
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight
-    const sectionTop = current.offsetTop - 50
-    sectionId = current.getAttribute("id")
-
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.add("active-link")
-    } else {
-      document
-        .querySelector(".nav__menu a[href*=" + sectionId + "]")
-        .classList.remove("active-link")
-    }
-  })
+  // const scrollY = window.pageYOffset
+  // sections.forEach((current) => {
+  //   const sectionHeight = current.offsetHeight
+  //   const sectionTop = current.offsetTop - 50
+  //   sectionId = current.getAttribute("id")
+  //   if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+  //     document
+  //       .querySelector(".nav__menu a[href*=" + sectionId + "]")
+  //       .classList.add("active-link")
+  //   } else {
+  //     document
+  //       .querySelector(".nav__menu a[href*=" + sectionId + "]")
+  //       .classList.remove("active-link")
+  //   }
+  // })
 }
 window.addEventListener("scroll", scrollActive)
 
